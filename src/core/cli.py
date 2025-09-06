3701b7414e3dc10602872d95998cc23e27ee92fa
FlynnQChen <121882506+FlynnQChen@users.noreply.github.com>
2025-09-06 19:58:26 +0800

    000

--- src/core/cli.py
+++ src/core/cli.py
@@ -1,7 +1,8 @@
 def main():
+    """主程序入口""" 
     from .strategy.base  import run_default_strategy 
     from src.api.binance_futures  import BinanceAPI
     run_default_strategy(BinanceAPI())
-
+ 
 if __name__ == "__main__":
     main()
